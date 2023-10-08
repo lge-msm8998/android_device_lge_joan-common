ca0bc779221a1995fe61ab9112f1b9ae54efbdf7
me-cafebabe <me.cafebabe@gmail.com>
2023-10-08 21:16:42 +0800

    joan-common: camera: Fix misleading indentation
    * According to the comment and the code, Both of the
      operations should be executed together if the condition
      is met, otherwise not
    * Hence, Put these two operations in the same { } block
    
    Change-Id: I99bf31c2bca994eff5cb98fddd736122bf01cf77

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -5447,9 +5447,10 @@ int QCamera3HardwareInterface::processCaptureRequest(
 
         // e.g. If we used video HDR in camcorder mode but are not using HDR in picture
         // mode, sensor HDR should be disabled here
-        if (!didSetSensorHdr)
+        if (!didSetSensorHdr) {
             setSensorHDR(mParameters, false, false);
-        mShouldSetSensorHdr = false;
+            mShouldSetSensorHdr = false;
+        }
 
         //TODO: validate the arguments, HSV scenemode should have only the
         //advertised fps ranges
